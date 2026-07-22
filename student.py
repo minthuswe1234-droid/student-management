3ada5629028be524d3fd5f604773528752d6c2f2
mts <minthuswe1234@gmail.com>
2026-07-22 11:28:55 +0630

    Add delete_student() function

--- student.py
+++ student.py
@@ -6,3 +6,7 @@ def add_student(name):
 def show_students():
     for student in students:
         print(student)
+
+def delete_student(name):
+    if name in students:
+        students.remove(name)        
